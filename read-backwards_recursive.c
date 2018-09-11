060e4e14d34907f9eb878a626a0194fc0aa91386
Justin Lex-Hammarskjöld <33381901+JustinLex@users.noreply.github.com>
2018-09-12 00:26:12 +0200

    add comments

--- read-backwards_recursive.c
+++ read-backwards_recursive.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
+
 int main() {
   processinput();
   return 0;
 }
 
+//print the characters before a newline recursively
 void processinput(void) {
   char newchar = getchar();
-  if(newchar == '\n')
+  if(newchar == '\n') // end recursion once we hit a newline
     return;
   else {
-    processinput();
-    printf(newchar);
+    processinput(); //recurse
+    printf(newchar); //print character on the way out of recursion
     return;
   }
 }
