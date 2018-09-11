3d496ddab63cb3ac72517d84f24894c823194f2b
Justin Lex-Hammarskjöld <33381901+JustinLex@users.noreply.github.com>
2018-09-12 00:59:41 +0200

    fix formatting bug

--- read-backwards_recursive.c
+++ read-backwards_recursive.c
@@ -5,17 +5,18 @@
 
 int main() {
   processinput();
+  printf("\n");
   return 0;
 }
 
 //print the characters before a newline recursively
 void processinput(void) {
-  char newchar = getchar();
+  int newchar = getchar();
   if(newchar == '\n') // end recursion once we hit a newline
     return;
   else {
     processinput(); //recurse
-    printf(newchar); //print character on the way out of recursion
+    printf(&newchar); //print character on the way out of recursion
     return;
   }
 }
